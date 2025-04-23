cc30e16ccb25e7cb7eb4e1ec38370d99a473870a
angel luna <angelstivengarcialuna@gmail.com>
2025-04-22 22:35:58 -0500

    Operador comparacion >=

--- src/operadores.rs
+++ src/operadores.rs
@@ -211,6 +211,10 @@ pub fn operadores_en_rust() {
     println!("Mayor o igual que flotantes: {}", mayor_igual_flotantes);
     // Salida: Mayor o igual que flotantes: true
 
+
+
+
+    
     // Operadores Bit a Bit
     // Operador  &
 
