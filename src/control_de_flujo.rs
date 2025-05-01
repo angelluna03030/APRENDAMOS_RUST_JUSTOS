149ae3b8318b7ce6b23a1a7ac89c14bbdbf31387
angel luna <angelstivengarcialuna@gmail.com>
2025-05-01 17:56:57 -0500

    control de flujo en RUST while

--- src/control_de_flujo.rs
+++ src/control_de_flujo.rs
@@ -1,5 +1,5 @@
 pub fn control_de_flujo() {
-    // control de flojo "if"
+    // control de flujo "if"
     // aqui estamos comparando si la variable tiene_licencia es igual a true
     let tiene_licencia = true;
     // si la variable tiene_licencia es igual a true, entonces se ejecuta el bloque de codigo
@@ -7,7 +7,7 @@ pub fn control_de_flujo() {
         println!("Juan puede conducir.");
     }
 
-    //control de flojo "else"
+    //control de flujo "else"
     // aqui estamos definindo la edad de juan
     let edad_juan: i8 = 17;
     // aqui estamos comparando si la edad de juan es mayor a 18; es una adulto
@@ -19,8 +19,7 @@ pub fn control_de_flujo() {
     }
     // salida : Juan no es un adulto.
 
-
-    //control de flojo "else if"
+    //control de flujo "else if"
     let edad:i8 = 18;
     // aqui estamos comparando si la edad de juan es menor a 18; es un adolescente
     if edad < 18 {
@@ -34,9 +33,22 @@ pub fn control_de_flujo() {
         println!("Juan es mayor de edad.");
     }
     // salida : Juan acaba de ser adulto.
+ 
+
+
+    //control de flujo "while"
+    // aqui estamos definiendo la variable contador y la inicializamos en 0
+    let mut contador:i8 = 15;
+    while contador > 9 {
+        // aqui estamos imprimiendo el valor de contador
+        println!("Contar hasta que sea 10: {}", contador);
+        // aqui estamos decrementando el valor de contador en 1
+        contador -= 1;
+    }
+    // salida : Contar hasta que sea 10: 15
 
 }
-    // salida : Juan es un adolescente.
+
 
 
 
