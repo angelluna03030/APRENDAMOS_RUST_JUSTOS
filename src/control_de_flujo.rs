d4662b015738c460aa64ee1fd620be5ac61a43a4
angel luna <angelstivengarcialuna@gmail.com>
2025-04-30 23:38:01 -0500

    control de flujo en RUST else if

--- src/control_de_flujo.rs
+++ src/control_de_flujo.rs
@@ -1,6 +1,4 @@
-pub fn control_de_flujo(){
-
-
+pub fn control_de_flujo() {
     // control de flojo "if"
     // aqui estamos comparando si la variable tiene_licencia es igual a true
     let tiene_licencia = true;
@@ -11,13 +9,34 @@ pub fn control_de_flujo(){
 
     //control de flojo "else"
     // aqui estamos definindo la edad de juan
-    let edad_juan:i8 = 17;
-    // aqui estamos comparando si la edad de juan es mayor a 18; es una adulto 
+    let edad_juan: i8 = 17;
+    // aqui estamos comparando si la edad de juan es mayor a 18; es una adulto
     if edad_juan >= 18 {
         println!("Juan es un adulto.");
     } else {
         // si la edad de juan no es mayor a 18, entonces se ejecuta el bloque de codigo
         println!("Juan no es un adulto.");
     }
+    // salida : Juan no es un adulto.
+
+
+    //control de flojo "else if"
+    let edad:i8 = 18;
+    // aqui estamos comparando si la edad de juan es menor a 18; es un adolescente
+    if edad < 18 {
+        // si la edad de juan es menor a 18, entonces se ejecuta el bloque de codigo
+        println!("Juan es menor de edad.");
+    } else if edad == 18 {
+        // si la edad de juan es igual a 18, entonces se ejecuta el bloque de codigo
+        println!("Juan acaba de ser adulto.");
+    } else {
+        // si la edad de juan no es menor a 18, entonces se ejecuta el bloque de codigo
+        println!("Juan es mayor de edad.");
+    }
+    // salida : Juan acaba de ser adulto.
 
 }
+    // salida : Juan es un adolescente.
+
+
+
